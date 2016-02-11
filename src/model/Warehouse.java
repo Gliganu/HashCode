5af17c9b61f420214dd40b860df377ed99244a5c
Andreea Stan <stan.andreea24@hotmail.com>
2016-02-11 22:20:51 +0200

    warehouse assignment

--- src/model/Warehouse.java
+++ src/model/Warehouse.java
@@ -1,6 +1,8 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * Created by GligaBogdan on 11-Feb-16.
@@ -8,10 +10,12 @@ import java.util.HashMap;
 public class Warehouse extends Piece {
 
     private HashMap<Integer,Integer> availableProductsMap;
+    private List<Order> orderList;
 
     public Warehouse(int x, int y) {
         super(x, y);
         this.availableProductsMap = new HashMap<>();
+        this.setOrderList(new ArrayList<>());
     }
 
     public HashMap<Integer, Integer> getAvailableProductsMap() {
@@ -20,5 +24,17 @@ public class Warehouse extends Piece {
 
     public void setAvailableProductsMap(HashMap<Integer, Integer> availableProductsMap) {
         this.availableProductsMap = availableProductsMap;
+    }
+
+    public List<Order> getOrderList() {
+        return orderList;
+    }
+
+    public void setOrderList(List<Order> orderList) {
+        this.orderList = orderList;
+    }
+
+    public void addOrderToWarehouse(Order order){
+        orderList.add(order);
     }
 }
